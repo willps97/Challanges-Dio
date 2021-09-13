e90c62648a49edbfa77bd2ac0f5faa20b1b33e2a
William Santos <william.sanctos@gmail.com>
2021-09-12 21:20:58 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alcool = 0;
+            int gasolina = 0;
+            int diesel = 0;
+            int option;
+
+            do
+            {
+                option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        alcool++;
+                        break;
+                    case 2:
+                        gasolina++;
+                        break;
+                    case 3:
+                        diesel++;
+                        break;
+                    case 4:
+                        Console.WriteLine("MUITO OBRIGADO");
+                        Console.WriteLine("Alcool: "+alcool);
+                        Console.WriteLine("Gasolina: "+gasolina);
+                        Console.WriteLine("Diesel: "+diesel);
+                        break;
+                    default:   
+                     continue;
+                }
+            } while (option != 4);
+        }
+    }
+}
