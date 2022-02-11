ffb26945c52b3a1956eee00fd0df1f990045ee9c
William Pereira <34622857+willps97@users.noreply.github.com>
2022-02-11 12:00:37 -0300

    Exercício bootcamp GFT 4
    Exercício que lê um número inteiro e retorna o seu quadrado e cubo.

---
+++ QuadradoEAoCubo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace QuadradoEAoCubo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            
+
+            for (int i = 1; i <= n; i++)
+            {
+                
+                int quadr = i * i;
+                int cubo = i * i * i;
+
+                Console.WriteLine(i + " "+quadr+" "+cubo);
+            }
+        }
+    }
+}
